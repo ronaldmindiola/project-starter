aa953d1026508c4a4c6f5ab5fbb57b4801067776
Ronald Mindiola <ronaldmindiola@live.com>
2025-06-06 14:48:14 -0500

    Shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [],
+  integrations: [react()],
 });
